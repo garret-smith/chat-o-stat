f09c4613af9f37ffcf3a94d41c88999a83b9c183
Garret Smith <garret.smith@gmail.com>
2014-12-22 07:13:15 +0000

    Use XMPP instead of twitter

--- cabinmeter.py
+++ cabinmeter.py
@@ -1,56 +1,76 @@
+# vim: et:sw=4:ts=4
 
+import logging
+import sleekxmpp
 import sys
-from PyQt4 import QtGui
+import threading
+import time
 
-from subprocess import check_output
 from enum import Enum
+from PyQt4 import QtGui
+from subprocess import check_output
 
-from twython import Twython
+import Adafruit_BMP.BMP085 as BMP085
 
 import access_codes
 
-def tweet():
-    api = Twython(access_codes.CONSUMER_KEY,
-		  access_codes.CONSUMER_SECRET,
-		  access_codes.ACCESS_KEY,
-		  access_codes.ACCESS_SECRET)
-    api.update_status(status="Twython test tweet!")
+if sys.version_info < (3, 0):
+    reload(sys)
+    sys.setdefaultencoding('utf8')
+else:
+    raw_input = input
+
+class ThermoBot(sleekxmpp.ClientXMPP):
+    def __init__(self, jid, password):
+        sleekxmpp.ClientXMPP.__init__(self, jid, password)
+        self.add_event_handler("session_start", self.start)
+        self.add_event_handler("message", self.message)
+
+    def start(self, event):
+        logging.info("chat bot got start event")
+        self.send_presence()
+        self.get_roster()
+
+    def message(self, msg):
+        logging.info("received message %s", msg)
+        if msg['type'] in ('chat', 'normal'):
+            msg.reply("Thanks for sending\n%(body)s" % msg).send()
 
 Mode = Enum('Mode', 'On Off Thermostat')
-Relay = Enum('Relay', 'Open Closed')
 
 class Thermostat():
-    """
-    Need some kind of delay on user actions, so that user can click something,
-    like "mode", and the action won't take effect for a second or so.
-    ie, give the user time to continue cycling the mode to the desired mode.
-    """
-
     def __init__(self, temp, setTemp):
         self._hysteresis = 1.0
+        self._stateLock = threading.Lock()
 
+        # each of these need to be observable variables
+        # they could be changed by multiple threads
+        # (UI / bot / temp poller) and UI needs notification
+        # notification when they do
         self._temp = float(temp)
         self._setTemp = float(setTemp)
         self._mode = Mode.Off
         self._heating = False
-        self._relay = Relay.Open
 
     def setMode(self, newMode):
-        self._mode = newMode
-        if self._mode == Mode.On:
-            self.turnOn()
-        elif self._mode == Mode.Off:
-            self.turnOff()
-        elif self._mode == Mode.Thermostat:
-            self.checkAction()
+        with self._stateLock:
+            self._mode = newMode
+            if self._mode == Mode.On:
+                self.turnOn()
+            elif self._mode == Mode.Off:
+                self.turnOff()
+            elif self._mode == Mode.Thermostat:
+                self.checkAction()
 
     def setSetTemp(self, setTemp):
-        self._setTemp = setTemp
-        self.checkAction()
+        with self._stateLock:
+            self._setTemp = setTemp
+            self.checkAction()
 
     def setTemp(self, temp):
-        self._temp = float(temp)
-        self.checkAction()
+        with self._stateLock:
+            self._temp = float(temp)
+            self.checkAction()
 
     def getMode(self):
         return self._mode
@@ -72,24 +92,22 @@ class Thermostat():
 
     def turnOn(self):
         if not self._heating:
-            self._relay = Relay.Closed
             self._heating = True
-            print "Turning on"
+            logging.info("Turning heat ON")
 
     def turnOff(self):
         if self._heating:
-            self._relay = Relay.Open
             self._heating = False
-            print "Turning off"
+            logging.info("Turning heat OFF")
 
 class ThermostatGui(QtGui.QWidget):
-    def __init__(self):
+    def __init__(self, thermostat):
         super(ThermostatGui, self).__init__()
-        self.thermostat = Thermostat(72.0, 78.0)
+        self.thermostat = thermostat
         self.initUI()
 
     def initUI(self):
-	self.setStyleSheet("""color: white;
+        self.setStyleSheet("""color: white;
 background-color: black;
 border-style: outset;
 border-color: grey;
@@ -97,7 +115,7 @@ border-width: 5px;
 border-radius: 10px;
 font: bold 16px;
 """)
-	btnStyleSheet = """
+        btnStyleSheet = """
 QPushButton:checked {
   background-color: qlineargradient(x1: 0, y1: 0, x2: 0, y2: 1, stop: 0 #dadbde, stop: 1 #f6f7fa);
   color: black
@@ -113,13 +131,13 @@ QPushButton:checked {
 
         onBtn = QtGui.QPushButton('On', self)
         onBtn.setCheckable(True)
-	onBtn.setStyleSheet(btnStyleSheet)
+        onBtn.setStyleSheet(btnStyleSheet)
         offBtn = QtGui.QPushButton('Off', self)
         offBtn.setCheckable(True)
-	offBtn.setStyleSheet(btnStyleSheet)
+        offBtn.setStyleSheet(btnStyleSheet)
         thermostatBtn = QtGui.QPushButton('Thermostat', self)
         thermostatBtn.setCheckable(True)
-	thermostatBtn.setStyleSheet(btnStyleSheet)
+        thermostatBtn.setStyleSheet(btnStyleSheet)
 
         modeGroup = QtGui.QButtonGroup(self)
         modeGroup.addButton(onBtn)
@@ -132,11 +150,11 @@ QPushButton:checked {
         offBtn.clicked.connect(self.offClicked)
         thermostatBtn.clicked.connect(self.thermostatClicked)
 
-	ip = check_output(["hostname", "-I"])
-	ipLabel = QtGui.QLabel(ip, self)
-	ipLabel.setStyleSheet("border-width: 0; border-radius: 0; font: 12px")
-	ipLabel.move(0, 0)
-	ipLabel.setFixedWidth(320)
+        ip = check_output(["hostname", "-I"])
+        ipLabel = QtGui.QLabel(ip, self)
+        ipLabel.setStyleSheet("border-width: 0; border-radius: 0; font: 12px")
+        ipLabel.move(0, 0)
+        ipLabel.setFixedWidth(320)
 
         layout = QtGui.QGridLayout()
 
@@ -174,9 +192,45 @@ QPushButton:checked {
     def thermostatClicked(self):
         self.thermostat.setMode(Mode.Thermostat)
 
+class TempPollerThread(threading.Thread):
+    def __init__(self, sensor, thermostat):
+        super(TempPollerThread, self).__init__()
+        self._sensor = sensor
+        self._thermostat = thermostat
+
+    def run(self):
+        while True:
+            tC = self._sensor.read_temperature()
+            tF = tC * (9.0/5.0) + 32.0
+            logging.info("tC: %f, tF: %f", tC, tF)
+            self._thermostat.setTemp(tF)
+            time.sleep(5)
+    
 def main():
+    logging.basicConfig(filename='log.txt', level=logging.DEBUG)
+    logging.info("cabinmeter starting")
+
+    sensor = BMP085.BMP085()
+    thermostat = Thermostat(72.0, 78.0)
+
+    tPollerThread = TempPollerThread(sensor, thermostat)
+    tPollerThread.start()
+
+    chatbot = ThermoBot(access_codes.jid, access_codes.password)
+    chatbot.register_plugin('xep_0030') # Service Discovery
+    chatbot.register_plugin('xep_0004') # Data Forms
+    chatbot.register_plugin('xep_0060') # PubSub
+    chatbot.register_plugin('xep_0199') # XMPP Ping
+    if chatbot.connect(('talk.google.com', 5222)):
+        logging.info("logged in to talk.google.com")
+        chatbot.process(block=False)
+    else:
+        logging.warning("login to talk.google.com failed")
+        sys.exit(0)
+
     app = QtGui.QApplication(sys.argv)
-    theromstat = ThermostatGui()
+    theromstatUi = ThermostatGui(thermostat)
+
     sys.exit(app.exec_())
 
 if __name__ == '__main__':
